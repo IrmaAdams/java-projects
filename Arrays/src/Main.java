d7de76b7245556ab5b46c89c3744823edb6512ad
IrmaAdams <strydom.irma@gmail.com>
2023-08-01 12:53:49 +0100

    single array

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // initialize array
+//        String[] artists = {"Van Gogh", "Monet", "Vermeer", "Degas"};
+//        artists[0] = "Renoir";
+//        System.out.println(artists[0]);
+//        System.out.println(artists[3]);
+
+        // create an array allocating size first and assigning values later
+        String[] artist = new String[3];
+        artist[0]="Van Gogh";
+        artist[1]="Degas";
+        artist[2]="Monet";
+        //System.out.println(artist[0]);
+
+        // using a for loop to iterate through all elements of an array
+        for(int i=0;i<= artist.length-1;i++){
+            System.out.println(artist[i]);
+        }
+
     }
 }
