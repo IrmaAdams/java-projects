30be72c9e5dd381a80fa12e78e408cf503e0a4b8
IrmaAdams <strydom.irma@gmail.com>
2023-08-02 14:43:07 +0100

    Practice Overloaded methods

--- Overloaded Methods/src/Main.java
+++ Overloaded Methods/src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // overloaded methods   =   methods that share the same name but have different parameters
+        //                          method name + parameters = method signature
+
+        double x = add(4.3, 7.0);
+        System.out.println(x);
+        int y = add(4, 7, 4);
+        System.out.println(y);
+        int z = add(4, 7,9,2);
+        System.out.println(z);
+
     }
+
+    static int add(int a, int b){
+        System.out.println("This is overloaded method Nr 1");
+        return a+b;
+    }
+    static int add(int a, int b, int c){
+        System.out.println("This is overloaded method Nr 2");
+        return a+b+c;
+    }
+    static int add(int a, int b, int c, int d){
+        System.out.println("This is overloaded method Nr 3");
+        return a+b+c+d;
+    }
+    static double add(double a, double b){
+        System.out.println("This is overloaded method Nr 4");
+        return a+b;
+    }
+    static double add(double a, double b, double c){
+        System.out.println("This is overloaded method Nr 5");
+        return a+b+c;
+    }
+    static double add(double a, double b, double c, double d){
+        System.out.println("This is overloaded method Nr 6");
+        return a+b+c+d;
+    }
+
 }
