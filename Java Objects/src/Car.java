1b27ad8c22900137b25c22c77fad990047af67e1
IrmaAdams <strydom.irma@gmail.com>
2023-08-03 13:20:38 +0100

    Practice objects

---
+++ Java Objects/src/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+
+    // Fields/attributes
+
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String colour = "blue";
+    double price = 50000.00;
+
+    // methods
+
+    void drive()
+    {
+        System.out.println("You drive the car.");
+    }
+
+    void brake(){
+        System.out.println("You step on the brakes.");
+    }
+}
--- Java Objects/src/Main.java
+++ Java Objects/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // object = an instance of a class that may contain attributes and methods
+        // example:  (sculpture, water bottle, speaker, coaster)
+
+        Car myCar = new Car();
+        System.out.println(myCar.colour);
+        System.out.printf("£ %,.2f", myCar.price);
+
+        myCar.brake();
+
+        Car mySecondCar = new Car();
+
     }
 }
