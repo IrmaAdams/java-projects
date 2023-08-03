f6f4eae526ef3760d60d104fe874f5a4be48cd8f
IrmaAdams <strydom.irma@gmail.com>
2023-08-03 16:52:50 +0100

    Practice overloaded constructors

--- Java Overloaded Constructors/src/Main.java
+++ Java Overloaded Constructors/src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // overloaded constructors =    multiple constructors within a class with the same name,
+        //                              but have different parameters
+        //                              name + parameters = signature
+
+        Pizza cheesePizza = new Pizza("Thin crust", "tomato", "mozzarella");
+        Pizza hawaiinPizza = new Pizza("Thin crust", "tomato", "mozzarella", "Pineapple");
+
+        System.out.println("Here are these ingredients for your pizza: ");
+        System.out.println(cheesePizza.bread);
+        System.out.println(cheesePizza.sauce);
+        System.out.println(cheesePizza.cheese);
+        System.out.println();
+
+        System.out.println("Here are these ingredients for your pizza: ");
+        System.out.println(hawaiinPizza.bread);
+        System.out.println(hawaiinPizza.sauce);
+        System.out.println(hawaiinPizza.cheese);
+        System.out.println(hawaiinPizza.topping);
 
     }
 }
