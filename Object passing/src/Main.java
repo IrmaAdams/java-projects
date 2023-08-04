8d5a68e60978c3eee3008015f1f89ff1023e4a30
IrmaAdams <strydom.irma@gmail.com>
2023-08-04 11:30:45 +0100

    Added notes on object passing

--- Object passing/src/Main.java
+++ Object passing/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // pass objects as arguments to a method
+
+        Garage garage = new Garage();
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Folkswagen Beatle");
+
+        garage.park(car1);
+        garage.park(car2);
+
     }
 }
