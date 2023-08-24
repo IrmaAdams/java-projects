7d0ae07717ba2dcb8d2f873e23c642b0f9dd07b2
IrmaAdams <strydom.irma@gmail.com>
2023-08-24 15:32:49 +0100

    practice code - File class in java

--- Java File Class/src/Main.java
+++ Java File Class/src/Main.java
@@ -1,6 +1,17 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        // file =   An abstract representation of file and directory path names
+
+        File file = new File("Test Text Document");
+
+        if(file.exists()){
+            System.out.println("This file exists!");
+        }else{
+            System.out.println("This file does not exist!");
+        }
+
     }
 }
