1a065f1ce5c34e5ddfb1a1b4bcb82d97d1f9ba35
IrmaAdams <strydom.irma@gmail.com>
2023-08-25 13:13:56 +0100

    added notes

--- Java audio/src/Main.java
+++ Java audio/src/Main.java
@@ -1,5 +1,20 @@
+import javax.sound.sampled.*;       // you can only use wav files with this import
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        File file = new File("owl.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+
+        clip.start();
+        String response = scanner.next();
+
     }
 }
