90379f4f870d80db66002728849b3a4723e8ea1e
IrmaAdams <strydom.irma@gmail.com>
2023-08-25 11:26:07 +0100

    Practiced writing to a file

--- Write to a file/src/Main.java
+++ Write to a file/src/Main.java
@@ -1,5 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // use the class - File writer class
+
+        try {
+            FileWriter writer = new FileWriter("poem.txt");
+            writer.write("First line of poem\n");
+            writer.write("Second line of poem\n");
+            writer.write("Third line of poem\n");
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
