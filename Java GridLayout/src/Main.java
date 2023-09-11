9eee8d44efb5f77b9c0733aef15c35f7658cb4e7
IrmaAdams <strydom.irma@gmail.com>
2023-09-11 14:03:13 +0100

    add notes to Java GridLayout

--- Java GridLayout/src/Main.java
+++ Java GridLayout/src/Main.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Layout Manager = Defines the natural layout for components within a container
+
+        // GridLayout =     places components in a grid of cells.
+        //                  Each component takes all the available space within its cell,
+        //                  and each cell is the same size.
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new GridLayout(2,2,10,10));
+
+        JButton button1 = new JButton("1");
+        frame.add(button1);
+        JButton button2 = new JButton("2");
+        frame.add(button2);
+        JButton button3 = new JButton("3");
+        frame.add(button3);
+        JButton button4 = new JButton("4");
+        frame.add(button4);
+
+        frame.setVisible(true);
     }
 }
