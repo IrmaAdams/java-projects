e6903ace9a9e796fd2472e6dd58af63c255767cf
IrmaAdams <strydom.irma@gmail.com>
2023-09-11 14:28:07 +0100

    add notes to Java LayeredPane

--- Java LayeredPane/src/Main.java
+++ Java LayeredPane/src/Main.java
@@ -1,5 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // JLayeredPane =   Swing container that provides a third dimension for positioning components
+        //                  ex. depth / Z-index (stacking components on top or underneath each other
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(50, 50, 200, 200);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.BLUE);
+        label2.setBounds(60, 60, 200, 200);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.GREEN);
+        label3.setBounds(70, 70, 200, 200);
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 500, 500);
+
+        layeredPane.add(label1, Integer.valueOf(0));
+        layeredPane.add(label2, Integer.valueOf(1));
+        layeredPane.add(label3, Integer.valueOf(2));
+
+        JFrame frame = new JFrame("JLayeredPane");
+        frame.add(layeredPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
     }
 }
