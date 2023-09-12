df5d535f8b7d536ad1d65a2475b48cd273dcc59c
IrmaAdams <strydom.irma@gmail.com>
2023-09-12 15:57:24 +0100

    notes

--- JOptionPane/src/Main.java
+++ JOptionPane/src/Main.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        // JOptionPane =    a pop-up standard dialog box that prompts users for a value
+        //                  or informs them of something.
+
+        //JOptionPane.showMessageDialog(null, "This is a plain message", "title", JOptionPane.PLAIN_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is an information message", "title", JOptionPane.INFORMATION_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is a Question message", "title", JOptionPane.QUESTION_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is a warning message", "title", JOptionPane.WARNING_MESSAGE);
+        //JOptionPane.showMessageDialog(null, "This is an error message", "title", JOptionPane.ERROR_MESSAGE);
+
     }
 }
