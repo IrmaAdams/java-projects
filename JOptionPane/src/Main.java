ddea7917e1bb9a046855da78cfe821a7b5ef67b4
IrmaAdams <strydom.irma@gmail.com>
2023-09-13 11:00:04 +0100

    added notes to JOptionPane

--- JOptionPane/src/Main.java
+++ JOptionPane/src/Main.java
@@ -12,5 +12,24 @@ public class Main {
         //JOptionPane.showMessageDialog(null, "This is a warning message", "title", JOptionPane.WARNING_MESSAGE);
         //JOptionPane.showMessageDialog(null, "This is an error message", "title", JOptionPane.ERROR_MESSAGE);
 
+        //System.out.println(JOptionPane.showConfirmDialog(null,"Do you code?", "Title", JOptionPane.YES_NO_CANCEL_OPTION));
+        //int answer = JOptionPane.showConfirmDialog(null,"Do you code?", "Title", JOptionPane.YES_NO_CANCEL_OPTION);
+
+        //JOptionPane.showInputDialog("What is your name?");
+
+//        String name = JOptionPane.showInputDialog("What is your name?");
+//        System.out.println("Hello "+name);
+
+        String[] responses = {"No, you're awesome!", "Thank you", "*Blush*"};
+        ImageIcon icon = new ImageIcon("smiley.jpeg");
+        JOptionPane.showOptionDialog(null,
+                "You are awesome!",
+                "Secret Message",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                icon,
+                responses,
+                0);
+
     }
 }
