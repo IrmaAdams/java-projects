d71a25a0e785caef118c0c22f62e6ae81fcb591b
IrmaAdams <strydom.irma@gmail.com>
2023-10-03 10:22:17 +0100

    added notes to Java Method Chaining

--- Java Method Chaining/src/Main.java
+++ Java Method Chaining/src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        // method chaining =    a common syntax for invoking multiple method calls in OOP
+        //                      it is useful for condensing code into fewer lines
+
+
+        // 3 lines of code:
+        String s1 = "   Hello ";
+
+        s1 = s1.concat("World!   ");
+        s1 = s1.toUpperCase();
+        s1 = s1.trim();
+
+        System.out.println(s1);
+
         
+        // 1 line of code:
+        String s2 = "   Hello ";
+
+        s2 = s2.concat("World!   ").toUpperCase().trim();
+
+        System.out.println(s2);
+
     }
 }
